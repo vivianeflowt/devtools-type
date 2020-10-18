1c0d67d2a9252e6590ddc0df10a6596e7a599962
vivianeflowt <vivianeflowt@gmail.com>
2020-10-18 18:52:03 -0300

    examples

--- examples/example.js
+++ examples/example.js
@@ -1,5 +1,8 @@
 const type = require('../index')
 
-console.log(type.is.string('aaaa'))
+console.log(type.typeOf('hello world'))
+console.log(type.typeOf({}))
+console.log(type.typeOf([]))
+console.log(type.is.string('123'))
 console.log(type.is.string(1))
 console.log(type.isNot.string(1))
